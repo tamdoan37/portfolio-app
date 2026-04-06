35fbeadebb1ea50249d3c736244166386b480757
tamdoan37 <157730907+tamdoan37@users.noreply.github.com>
2026-04-05 22:10:09 -0500

    Update API URL and change endpoint for projects

--- src/app/services/portfolio.service.ts
+++ src/app/services/portfolio.service.ts
@@ -5,14 +5,15 @@ import { Project, ContactResponse } from '../models/portfolio.models';
 @Injectable({ providedIn: 'root' })
 export class PortfolioService {
   private http = inject(HttpClient);
-  private API_URL = 'http://localhost:3000/api';
+  //private API_URL = 'https://localhost:7203/api';
+  private API_URL = 'https://portfolio-api-dotnet.onrender.com/api';
 
   // Signals for reactive state
   projects = signal<Project[]>([]);
   pwaStatus = signal('PWA is Online and Registered successfully!');
 
   loadProjects() {
-    this.http.get<Project[]>(`${this.API_URL}/projects`).subscribe(data => {
+    this.http.get<Project[]>(`${this.API_URL}/portfolios`).subscribe(data => {
       this.projects.set(data);
     });
   }
